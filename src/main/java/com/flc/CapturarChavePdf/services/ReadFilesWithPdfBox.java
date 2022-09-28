4b61bd6128944a34581ea972de775bb0a709ae04
fernando.correa <paracorrea@gmail.com>
2022-09-28 12:55:22 -0300

    continuidade1

--- src/main/java/com/flc/CapturarChavePdf/services/ReadFilesWithPdfBox.java
+++ src/main/java/com/flc/CapturarChavePdf/services/ReadFilesWithPdfBox.java
@@ -5,10 +5,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class ReadFilesWithPdfBox {
 
-
+	private static List<String> filesList = new ArrayList<>();
+	
+	
 	public static void findAllFiles(String file ) throws IOException {
 		
 		File file1 = new File(file);
@@ -18,6 +28,28 @@ public class ReadFilesWithPdfBox {
 		
 		try (BufferedReader br = new BufferedReader(reader)) {
 			String linha = br.readLine();
+			
+
+			while(linha != null) {
+			   // LOG.info("Linha capturada: "+linha);
+			    linha = br.readLine();
+			    
+			    System.out.println("primeiro PDF = "+linha.toString());
+			    PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(linha));
+	            parser.parse();
+	            COSDocument cosDoc = parser.getDocument();
+	            PDFTextStripper pdfStripper = new PDFTextStripper();
+	            PDDocument pdDoc = new PDDocument(cosDoc);
+	            for (int i = 1; i <= pdDoc.getNumberOfPages(); i++) {
+	                pdfStripper.setStartPage(i);
+	                pdfStripper.setEndPage(i);
+	                String parsedText = pdfStripper.getText(pdDoc);
+	                System.out.println("Página " + i + ": " + parsedText);
+			    
+	            }
+			    
+			    
+			}
 		}
 		
 		
