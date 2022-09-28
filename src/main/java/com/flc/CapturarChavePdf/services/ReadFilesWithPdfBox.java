007ffc5d47e77a42502ef49ce8cae9a6b3d5be7f
fernando.correa <paracorrea@gmail.com>
2022-09-28 12:21:40 -0300

    avancando na captura do arquivo

--- src/main/java/com/flc/CapturarChavePdf/services/ReadFilesWithPdfBox.java
+++ src/main/java/com/flc/CapturarChavePdf/services/ReadFilesWithPdfBox.java
@@ -1,16 +1,39 @@
 package com.flc.CapturarChavePdf.services;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ReadFilesWithPdfBox {
 
-	String linha;
+
+	public static void findAllFiles(String file ) throws IOException {
+		
+		File file1 = new File(file);
+		
+		FileInputStream stream = new FileInputStream(file1);
+		InputStreamReader reader = new InputStreamReader(stream);
+		
+		try (BufferedReader br = new BufferedReader(reader)) {
+			String linha = br.readLine();
+		}
+		
+		
+		
+		
+		
+	}
 	
 	
 	
 	
 	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		String file = new String("C:\\folders\\output.txt");
+		findAllFiles(file);
 
 	}
 
